eb70f3b4844722528378684b2bd5a25395acf84f
Ismo Harjunmaa <ismo.harjunmaa@haaga-helia.fi>
2026-03-24 12:36:41 +0200

    Package update

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,12 +3,12 @@
 
 ***REMOVED***
 ***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
-***REMOVED***
+  apiKey: "AIzaSyDqr7jr8ZR7UBODTWX8jDFe3owASw6m3L8",
+  authDomain: "pilvipalvelut-1eeda.firebaseapp.com",
+  projectId: "pilvipalvelut-1eeda",
+  storageBucket: "pilvipalvelut-1eeda.firebasestorage.app",
+  messagingSenderId: "1087719224135",
+  appId: "1:1087719224135:web:370cdc0cfbfc0765ca1262"
 ***REMOVED***
 
 ***REMOVED***
